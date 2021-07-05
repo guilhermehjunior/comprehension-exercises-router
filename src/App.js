64bfc90e5f27f2c32c17ddbc038fd48da7f9956d
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-05 16:03:26 -0300

    adicionando props ao users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route }from 'react-router-dom';
+import { BrowserRouter, Route, Link }from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
@@ -8,9 +8,14 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/About">About</Link>
+          <Link to="/users">Users</Link>
+        </nav>
         <Route exact path="/" component={ Home }/>
         <Route path="/about" component={ About }/>
-        <Route path="/users" component= { Users } />
+        <Route path="/users/:id" render= {(props) => <Users {...props} greetingsMessage="Good Morning" />} />
       </BrowserRouter>
     ); 
   }
