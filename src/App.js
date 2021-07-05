0a76363a6881d42e8a001bd35014f3b6ccde435b
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-05 15:52:33 -0300

    adicionando User and ABOUT route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter }from 'react-router-dom';
+import { BrowserRouter, Route }from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        <Route exact path="/" component={ Home }/>
+        <Route path="/about" component={ About }/>
+        <Route path="/users" component= { Users } />
       </BrowserRouter>
     ); 
   }
